99150789176af06b823456050fb3d578cbca3476
Gly <gabin.lefranc@gmail.com>
2022-12-14 01:55:59 +0100

    WIP Update from test on Ubuntu

--- init.sh
+++ init.sh
@@ -18,13 +18,33 @@ function quit_if_failed {
         exit 1
     fi
 }
+function download {
+    if [ "$(which wget)" != "" ]; then
+        wget -O - "$1"
+    elif [ "$(which curl)" != "" ]; then
+        curl -fsSL "$1"
+    fi
+}
+
+function install_brew {
+    if [ "$(which curl)" == "" ]; then
+        echo "Installing curl..."
+        $INSTALL_PKG curl
+    fi
+    echo "Installing brew..."
+    bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    if [ "$(uname)" == "Linux" ]; then
+        echo '# Set PATH, MANPATH, etc., for Homebrew.' >> $HOME/.profile
+        echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> $HOME/.profile
+        eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+    fi
+}
 
 # select a package manager
 if [ "$(uname)" == "Darwin" ]; then
     # On macOS, install brew if it's not already installed
     if [ "$(which brew)" == "" ]; then
-        echo "Installing brew..."
-        /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+        install_brew
     fi
     echo "Using brew as package manager..."
     PKG_MGR="brew"
@@ -37,37 +57,37 @@ elif [ "$(uname)" == "Linux" ]; then
         PKG_MGR="apt"
         INSTALL_PKG="sudo $PKG_MGR install"
         echo "Updating apt..."
-        sudo $INSTALL_PKG update && $INSTALL_PKG upgrade || quit_if_failed "apt update && apt upgrade"
+        sudo $PKG_MGR update && sudo $PKG_MGR upgrade -y || quit_if_failed "apt update && apt upgrade"
     elif [ "$(which apt-get)" != "" ]; then
         echo "Using apt-get as package manager..."
         PKG_MGR="apt-get"
         INSTALL_PKG="sudo $PKG_MGR install"
         echo "Updating apt-get..."
-        sudo $INSTALL_PKG update && $INSTALL_PKG upgrade || quit_if_failed "apt-get update && apt-get upgrade"
+        sudo $PKG_MGR update && sudo $PKG_MGR upgrade || quit_if_failed "apt-get update && apt-get upgrade"
     elif [ "$(which yum)" != "" ]; then
         echo "Using yum as package manager..."
         PKG_MGR="yum"
         INSTALL_PKG="sudo $PKG_MGR install"
         echo "Updating yum..."
-        sudo $INSTALL_PKG update && $INSTALL_PKG upgrade || quit_if_failed "yum update && yum upgrade"
+        sudo $PKG_MGR update && sudo $PKG_MGR upgrade || quit_if_failed "yum update && yum upgrade"
     elif [ "$(which dnf)" != "" ]; then
         echo "Using dnf as package manager..."
         PKG_MGR="dnf"
         INSTALL_PKG="sudo $PKG_MGR install"
         echo "Updating dnf..."
-        sudo $INSTALL_PKG update && $INSTALL_PKG upgrade || quit_if_failed "dnf update && dnf upgrade"
+        sudo $PKG_MGR update && sudo $PKG_MGR upgrade || quit_if_failed "dnf update && dnf upgrade"
     elif [ "$(which pacman)" != "" ]; then
         echo "Using pacman as package manager..."
         PKG_MGR="pacman"
         INSTALL_PKG="sudo $PKG_MGR -S"
         echo "Updating pacman..."
-        sudo $INSTALL_PKG -Syu
+        sudo $PKG_MGR -Syu
     elif [ "$(which brew)" != "" ]; then
         echo "Using brew as package manager..."
         PKG_MGR="brew"
         INSTALL_PKG="$PKG_MGR install"
         echo "Updating brew..."
-        $INSTALL_PKG update && $INSTALL_PKG upgrade || quit_if_failed "brew update && brew upgrade"
+        $PKG_MGR update && $PKG_MGR upgrade || quit_if_failed "brew update && brew upgrade"
     else
         echo "No package manager found!"
         exit 1
@@ -98,20 +118,33 @@ fi
 
 # download and install nu shell
 if [ "$(which nu)" == "" ]; then
+    # install brew for linux if needed
+    if [ "$(which pacman)" == "" ] && [ "$(which brew)" == "" ]; then
+        install_brew
+    fi
     echo "Installing nu shell..."
-    $INSTALL_PKG nushell
-    # ask for setting nu as default shell
-    echo "Do you want to set nu as your default shell? [y/N]"
-    read -r SET_NU_AS_DEFAULT
-    if [ "$SET_NU_AS_DEFAULT" == "y" ]; then
-        echo "Setting nu as default shell..."
-        sudo chsh -s "$(which nu)"
+    INSTALL=KO
+    if [ "$(which pacman)" != "" ]; then
+        sudo pacman -S nushell && INSTALL=OK
+    elif [ "$(which brew)" != "" ]; then
+        brew install nushell && INSTALL=OK
+    fi
+    if [ $INSTALL != OK ]; then
+        echo "Unable to install nushell. Skip..."
+    else
+        # ask for setting nu as default shell
+        echo "Do you want to set nu as your default shell? [y/N]"
+        read -r SET_NU_AS_DEFAULT
+        if [ "$SET_NU_AS_DEFAULT" == "y" ]; then
+            echo "Setting nu as default shell..."
+            sudo chsh -s "$(which nu)"
+        fi
     fi
 fi
 
 # install dot files and folders
 echo "Installing dot files and folders..."
-cp .cache .cargo .config .p10k.zsh .zshrc  ~/
+cp -r .cache .cargo .config .zshrc  ~/
 
 #ask for starship prompt or powerlevel10k
 echo "Which prompt do you want to install ?"
@@ -131,8 +164,8 @@ case $PROMPT in
             elif [ "$(which brew)" != "" ]; then
                 $INSTALL_PKG starship
             else
-                echo "Installing starship prompt using curl..."
-                curl -fsSL https://starship.rs/install.sh | sh
+                echo "Installing starship prompt from starship.rs/install.sh..."
+                download https://starship.rs/install.sh | sh
             fi
         else
             echo "Starship prompt already installed!"
@@ -145,14 +178,14 @@ case $PROMPT in
         ;;
     2)
         echo "Installing zimfw..."
-        curl -fsSL https://raw.githubusercontent.com/zimfw/install/master/install.zsh | zsh
+        download https://raw.githubusercontent.com/zimfw/install/master/install.zsh | zsh
         echo "Installing powerlevel10k..."
-        cp setup/.p10k.zsh ~/
         cat << EOF >> ~/.zimrc
 # Use powerlevel10k theme
 zmodule romkatv/powerlevel10k --use degit
 EOF
         zimfw install
+        cp setup/.p10k.zsh ~/
         cat setup/powerlevel10k_zshrc.sh >> ~/.zshrc
         ;;
     0)
